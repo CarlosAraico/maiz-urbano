c6a7722ad1c2883fb6fa852b711ef11310e3e210
CarlosAraico <90725944+CarlosAraico@users.noreply.github.com>
2025-11-30 03:41:38 -0600

    Mover index.html y assets al root para GitHub Pages

--- frontend/src/utils/scale.js
+++
@@ -1,4 +0,0 @@
-export const scaleAmount = (quantity, baseServings, target) => {
-  if (!baseServings || baseServings <= 0) throw new Error('baseServings must be > 0');
-  return Math.round((quantity / baseServings) * target);
-};
