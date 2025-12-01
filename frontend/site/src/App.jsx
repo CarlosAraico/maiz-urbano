7d85d037cca205951bf99db28efda7baf5176c8b
CarlosAraico <90725944+CarlosAraico@users.noreply.github.com>
2025-11-30 19:58:21 -0600

    fix: enable SPA routing with basename

--- frontend/site/src/App.jsx
+++ frontend/site/src/App.jsx
@@ -1,23 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home.jsx";
-import Carrito from "./pages/Carrito.jsx";
-import Mapa from "./pages/Mapa.jsx";
-import Sucursales from "./pages/Sucursales.jsx";
-import Facturacion from "./pages/Facturacion.jsx";
-import Preview from "./pages/Preview.jsx";
-import DsPreview from "./pages/DsPreview.jsx";
+import Home from "./Home.jsx";
+import Carrito from "./Carrito.jsx";
+import Facturacion from "./Facturacion.jsx";
+import Mapa from "./Mapa.jsx";
+import Preview from "./Preview.jsx";
+import DsPreview from "./DsPreview.jsx";
+import Sucursales from "./Sucursales.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/carrito" element={<Carrito />} />
-      <Route path="/mapa" element={<Mapa />} />
-      <Route path="/sucursales" element={<Sucursales />} />
       <Route path="/facturacion" element={<Facturacion />} />
+      <Route path="/mapa" element={<Mapa />} />
       <Route path="/preview" element={<Preview />} />
       <Route path="/ds-preview" element={<DsPreview />} />
+      <Route path="/sucursales" element={<Sucursales />} />
     </Routes>
   );
 }
